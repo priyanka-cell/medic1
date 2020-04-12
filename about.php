ee8a0bedc45a3723bc1811375dd571b493d4ca6b
priyanka <pdkshirsagar@mitaoe.ac.in>
2020-04-11 20:51:42 -0700

    first commit

---
+++ about.php
@@ -0,0 +1,70 @@
+<?php include('header.php'); ?>
+
+
+
+
+
+	<!-- this is for donor registraton -->
+	<div class="main_content" style="background-color:#fff;">
+		<h3 class="text-center" style="background-color:#272327;color: #fff;">About Us</h3>
+		<div class="col-md-12">
+			<div class="col-md-6">
+				<img src="img/aboutimag33.jpg" alt="" class="img-responsive">
+			</div>
+
+			<div class="col-md-6">
+				<article>
+					<h3 style="color:#0616BC;">We are Beside you</h3>
+					<p class="text-justify">
+	To extend equally to all Indians, regardless of their ability to pay, comprehensive health services of the highest quality in an atmosphere of humane care, dignity, and respect.
+To promote and protect, as both innovator and advocate, the health, welfare, and safety of the people in India.
+To join with other health workers and with communities in a partnership which will enable each of our institutions to promote and protect health in its fullest sense  the total physical, mental and social well-being of the people.Health + Hospitals vision is to be a fully integrated health system that enables Indians to live their healthiest lives.
+
+Our ValuesHealth + Hospitals has established the ICARE standards for all our staff. It will help us offer our patients a better experience when under our care and will increase staff awareness to become better engaged with the mission and vision.
+</p>
+				</article>
+			</div>
+		</div>
+
+
+		<div class="col-md-12">
+			<div class="col-md-6">
+				<article>
+					<h3 style="color:#0616BC;">24/7 hour service</h3>
+					<p class="text-justify">We are providing advanced emergency services with rapid response for any kind of treatment. We have well-equipped emergency and trauma center with facilities of international standards. Our emergency departments are always ready for these services. The department has state-of-the-art equipment and advanced patient monitoring systems to respond to any medical emergency.
+The hospitals provides round-the-clock emergency services, with a facility that is fully equipped and managed by a dedicated team of qualified emergency care professionals. A well-defined emergency management system has been developed to increase efficiency of rescue efforts. This includes enhanced first aid on site and improved overall coordination amongst the hospitals staff involved in coping with emergency situations. We have developed a special training module for every emergency that may arise. The highlights of our training are as follows:
+</br>o	Basic life support training is given regularly to nurses and paramedical staff
+</br>o	Advanced life support and advanced training in trauma management is given to doctors and ambulance staff
+ .</p>
+				</article>
+			</div>
+			<div class="col-md-6">
+				<img src="img/aboutimag11.jpg" alt="" class="img-responsive"><br>
+			</div>
+		</div>
+          
+    </div>
+		
+	
+	
+	
+
+	
+ <?php include('footer.php'); ?>
+
+
+	
+	</div><!--  containerFluid Ends -->
+
+
+
+
+	<script src="js/bootstrap.min.js"></script>
+
+
+
+
+
+	
+</body>
+</html>
---
+++ medical_college.php
@@ -0,0 +1,88 @@
+<?php include('header.php'); ?>
+
+
+	<!-- mc info -->
+	<div class="form-group mc">
+		<h2 class="text-center" style="background-color:#272327;color: #fff;">Government Medical College in INDIA</h2>
+
+                              <table cellpadding="10" cellspacing="10" class="table table-hover">   
+
+                                  <tr>
+                                      <th>No.</th><th>Name</th><th>Acronym</th><th>Established</th><th>Location</th><th>website</td>
+                                  </tr>
+                                  <tr>
+                                      <td>01</td><td>Andhra Medical College</td><td>AMC</td><td>1923</td><td>Visakhapatnam</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>02</td><td>Sri Venkateswara Medical College</td><td>SVMC</td><td>1960</td><td>Tirupati</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>03</td><td>Sri Venkateswara Institute of Medical Sciences</td><td>SVIMS</td><td>1993</td><td>Tirupati</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>04</td><td>Government Medical College, Srikakulam</td><td>SGMC</td><td>2008</td><td>Srikakulam</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>05</td><td>Armed Forces Medical College</td><td>AFMC</td><td>1948</td><td>Pune</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>06</td><td>B.J. Medical College</td><td>BJMC</td><td>1878</td><td>Pune</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>07</td><td>Indira Gandhi Government Medical College & Hospital</td><td>IGGMCH</td><td>1968</td><td>Nagpur</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>08</td><td>Government Medical College & General Hospital Baramati</td><td>-</td><td>2019</td><td>Baramati</td><td><a href="#">website</a></td>
+ 
+                                 </tr>
+                                  <tr>
+     
+<td>09</td><td>Government Medical College, Latur</td><td>GMCL</td><td>2001</td><td>Latur</td><td><a href="#">website</a></td>
+                                  </tr>
+                              
+    <tr>
+                                      <td>10</td><td>Government Medical College, Miraj </td><td>GMC Miraj</td><td>1962</td><td>Miraj</td><td><a href="#">website</a></td>
+                                  </tr>
+     
+                             <tr>
+                                      <td>11</td><td>Government Medical College and Hospital, Nagpur</td><td>GMC Nagpur</td><td>1947</td><td>Nagpur</td><td><a href="#">website</a></td>
+                                  </tr>
+    
+                              <tr>
+                                      <td>12</td><td>Grant Government Medical College</td><td>Grant medical college</td><td>1845</td><td>Mumbai</td><td><a href="#">website</a></td>
+                                  </tr>
+      
+                            <tr>
+                                      <td>13</td><td>Rajarshee Chattrapati Shahu Maharaj Govt. Medical College and CPR Hospital</td><td>RCSMGMC</td><td>2000</td><td>Kolhapur</td><td><a href="#">website</a></td>
+                                  </tr>
+             
+  <tr>
+                                      <td>14</td><td>Shri Bhausaheb Hire Government Medical College</td><td>SBHGMC</td><td>1989</td><td>Dhule</td><td><a href="#">website</a></td>
+                                  </tr>
+             
+<tr>
+                                      <td>15</td><td>Shri Vasantrao Naik Government Medical College and Hospital</td><td>SVNGMC</td><td>1989</td><td>Yavatmal</td><td><a href="#">website</a></td>
+                                  </tr>
+  
+                         
+   
+
+                                                   
+                              </table>
+	</div>
+	
+
+    <!-- footer section --> 
+			 <?php include('footer.php'); ?>
+		<!-- footer section Ends--> 
+
+
+	
+	</div><!--  containerFluid Ends -->
+
+
+
+	<script src="js/bootstrap.min.js"></script>
+	
+</body>
+</html>
